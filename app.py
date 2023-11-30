7a838565d9d17899cd97e5a1ae69c7ed2df21416
Taoyu <2646497282@qq.com>
2023-11-30 23:10:44 +0800

    Create, edit and delete item by form

--- app.py
+++ app.py
@@ -36,7 +36,6 @@ def inject_user():  # 函数名可以随意修改
     return dict(user=user)  # 需要返回字典，等同于return {'user': user}
 
 
-
 import click
 
 
@@ -78,10 +77,57 @@ def initdb(drop):
     click.echo('Initialized database.')  # 输出提示信息
 
 
-@app.route('/')
+from flask import request, url_for, redirect, flash
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    movies = Movie.query.all()  # 读取所有电影记录
-    return render_template('index.html',  movies=movies)
+    if request.method == 'POST':
+        title = request.form.get('title')  # 传入表单对应输入字段的name 值
+        year = request.form.get('year')
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')  # 显示错误提示
+            return redirect(url_for('index'))  # 重定向回主页
+            # 保存表单数据到数据库
+            movie = Movie(title=title, year=year)  # 创建记录
+            db.session.add(movie)  # 添加到数据库会话
+            db.session.commit()  # 提交数据库会话
+            flash('Item created.')  # 显示成功创建的提示
+            return redirect(url_for('index'))  # 重定向回主页
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', user=user, movies=movies)
+
+
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == 'POST':  # 判断是否是 POST 请求
+        # 获取表单数据
+        title = request.form('title')  # 传入表单对应输入字段的name 值
+        year = request.form('year')
+        # 验证数据
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')  # 显示错误提示
+            return redirect(url_for('edit', movie_id=movie_id))  # 重定向回主页
+        # 保存表单数据到数据库
+        movie.title = title  # 更新标题
+        movie.year = year  # 更新年份
+        db.session.commit()  # 提交数据库会话
+        flash('Item updated.')  # 显示成功创建的提示
+        return redirect(url_for('index'))  # 重定向回主页
+
+    return render_template('edit.html', movie=movie)
+
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])  # 限定只接受 POST 请求
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)  # 获取电影记录
+    db.session.delete(movie)  # 删除对应的记录
+    db.session.commit()  # 提交数据库会话
+    flash('Item deleted.')
+    return redirect(url_for('index'))  # 重定向回主页
 
 
 @app.errorhandler(404)  # 传入要处理的错误代码
